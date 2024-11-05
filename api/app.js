53cbe182930f2356a3c8d976ed6160e067d91b57
José Romualdo <joseromualdo@outlook.com>
2024-11-05 06:31:25 -0300

    add code to source control

---
+++ api/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const promBundle = require('express-prom-bundle');
+
+const app = express();
+
+promBundle.normalizePath = (req, opts) => {
+    return req.route?.path ?? "No";
+};
+// Init metrics
+const metricsMiddleware = promBundle({
+    includeMethod: true,
+    includePath: true,
+    buckets: [0.001, 0.01, 0.1, 1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 50, 70, 100, 200],
+    customLabels: { model: "No" },
+    transformLabels: (labels, req, res) => {
+        labels.model = req?.body?.model ?? req?.body?.imageModel ?? req?.body?.voice ?? "No";
+        return labels;
+    },
+});
+app.use(metricsMiddleware);
+// Routes 
+app.get('/data', (req, res) => {
+  res.json({data: 'Some data...'});
+}); 
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
